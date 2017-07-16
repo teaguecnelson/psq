961fbd0963210c3b7fe66bf4276f7eccfbe9c9af
teaguecnelson <teaguecnelson@gmail.com>
2017-07-16 12:36:42 -0400

    working counters and results

---
+++ src/components/Previous.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const Previous = () => (
+	if (i>0) {
+		<button className="btn" onClick={this.decrementCount}>Previous Question</button>
+	}
+)
+
--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { QuizMeta } from './QuizMeta'
 import { MidResult } from './MidResult'
 import { Question } from './Question'
+import { Results } from './Results'
 
 export class App extends React.Component {
+
 	constructor(props) {
 		super(props)
 		this.state = {
+			i: 0,
 			allQuestions: [
 			{
 				question: "Question 01",
@@ -28,25 +31,55 @@ export class App extends React.Component {
 				answer01: "Answer 3.1",
 				answer02: "Answer 3.2",
 				answer03: "Answer 3.3"
+			},
+			{
+				question: "Question 01",
+				description: "Question 01's description",
+				answer01: "Answer 1.1",
+				answer02: "Answer 1.2",
+				answer03: "Answer 1.3"
 			}
 		]
 		}
+		// This binding is necessary to make `this` work in the callback
+    	this.incrementCount = this.incrementCount.bind(this);
+    	this.decrementCount = this.decrementCount.bind(this);
 	}
 	countQuestions() {
 		return this.state.allQuestions.length
 	}
+	incrementCount() {
+	    this.setState({
+	      i: this.state.i + 1
+	    });
+	}
+	decrementCount() {
+	    this.setState({
+	      i: this.state.i - 1
+	    });
+	}
 	render() {
 		const allQuestions = this.state.allQuestions
+		let button = null;
+	    if (this.state.i>0) {
+	      button = <button className="btn" onClick={this.decrementCount}>Previous Question</button>;
+	    }
+	    if (this.state.i + 1>this.countQuestions()) {
+	      return <Results />;
+	  	}
 		return (
 			<div>
 			<QuizMeta total={this.countQuestions()}
-					  currentQuestion={ 2+1 } />
+					  currentQuestion={ this.state.i + 1 } />
 			<MidResult />
-			<Question question={ this.state.allQuestions[2]["question"] }
-					  description={ this.state.allQuestions[2]["description"] }
-					  answer01={ this.state.allQuestions[2]["answer01"] }
-					  answer02={ this.state.allQuestions[2]["answer02"] }
-					  answer03={ this.state.allQuestions[2]["answer03"] } />
+			<Question question={ this.state.allQuestions[this.state.i]["question"] }
+					  description={ this.state.allQuestions[this.state.i]["description"] }
+					  answer01={ this.state.allQuestions[this.state.i]["answer01"] }
+					  answer02={ this.state.allQuestions[this.state.i]["answer02"] }
+					  answer03={ this.state.allQuestions[this.state.i]["answer03"] } />
+			<button className="btn" onClick={this.incrementCount}>Increment</button>
+			{Object.keys(this.state.allQuestions[2]).length}
+			{button}
 			</div>
 		)
 	}
